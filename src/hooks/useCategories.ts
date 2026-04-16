3be44d3a622eb2f3aee7ac9558b2d7c2f9dc6aee
habitatfloricultura-lgtm <evertonapk09@gmail.com>
2026-04-15 22:33:12 -0300

    ajuste filter

--- src/hooks/useCategories.ts
+++ src/hooks/useCategories.ts
@@ -8,12 +8,11 @@ export type Category = {
 
 const fetchCategories = async (): Promise<Category[]> => {
   const res = await fetch(
-    "https://vkpmrgpwdxcgmlrruptz.supabase.co/functions/v1/get-categories"
+    "https://vkpmrgpwdxcgmlrruptz.supabase.co/functions/v1/get-categories",
   );
   if (!res.ok) throw new Error("Erro ao buscar categorias");
   return res.json();
 };
-
 export const useCategories = () => {
   return useQuery({
     queryKey: ["categories"],
